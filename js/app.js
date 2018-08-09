a5cfd6b45548ae92b75abfe3ce50c16250da2373
Dillon Foley <foleydil@gmail.com>
2018-08-08 21:30:51 -0400

    feat: Add click listener and counter to page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
+/*** GLOBAL VARIABLES ***/
+
+let clickCount = 0;
+
 /*** FUNCTION DECLARATIONS ***/
 
 function init() {
-
+  // Add click listener to image, increment clickCount and update counter on click
+  const catPic = document.querySelector('#catImage');
+  const counter = document.querySelector('.counter');
+  catPic.addEventListener('click', function() {
+    clickCount += 1;
+    counter.innerHTML = clickCount;
+  });
 }
 
 /*** CODE RUNS ON PROGRAM LOAD ***/
