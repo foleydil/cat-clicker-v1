d312e4a18ca37f212dbff715503b453c95af507b
Dillon Foley <foleydil@gmail.com>
2018-08-26 17:50:30 -0400

    feat: Add click functionality for 2nd cat

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
 /*** GLOBAL VARIABLES ***/
 
-let clickCount = 0;
+let cat1ClickCount = 0;
+let cat2ClickCount = 0;
 
 /*** FUNCTION DECLARATIONS ***/
 
 function init() {
   // Add click listener to image, increment clickCount and update counter on click
-  const catPic = document.querySelector('#cat1Image');
-  const counter = document.querySelector('#cat1-counter');
-  catPic.addEventListener('click', function() {
-    clickCount += 1;
-    counter.innerHTML = "Click Count: " + clickCount.toLocaleString();
+  const cat1Pic = document.querySelector('#cat1Image');
+  const cat1Counter = document.querySelector('#cat1-counter');
+  cat1Pic.addEventListener('click', function() {
+    cat1ClickCount += 1;
+    cat1Counter.innerHTML = "Click Count: " + cat1ClickCount.toLocaleString();
+  }, false);
+
+  // Add click listener to image, increment clickCount and update counter on click
+  const cat2Pic = document.querySelector('#cat2Image');
+  const cat2Counter = document.querySelector('#cat2-counter');
+  cat2Pic.addEventListener('click', function() {
+    cat2ClickCount += 1;
+    cat2Counter.innerHTML = "Click Count: " + cat2ClickCount.toLocaleString();
   }, false);
 }
 
